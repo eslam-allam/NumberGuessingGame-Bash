6fa8c31dcf9d619d6670fb261ca67beb776d02fc
eslam-allam <eslamallam73@gmail.com>
2022-07-24 09:40:33 +0000

    feat: added program layout

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Login screen
+LOGIN_SCREEN () {
+  # Ask for username
+
+  # If username less than 22 characters
+
+    # Return to login with error message
+
+  # Check username in database
+
+  # If doesn't exist
+
+    # Add to database
+  
+  # Fetch user_id, games_played, best_game from database
+
+  # Display user information
+
+  # Go to game
+}
